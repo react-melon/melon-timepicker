c08250ba3cf3db0e319a1ed1fce7eb5f608edfec
lupengyu <lupengyu@baidu.com>
2016-12-12 15:21:44 +0800

    update react & react-dom version

--- tools/karma/config.js
+++ tools/karma/config.js
@@ -44,7 +44,7 @@ module.exports = {
                 },
                 // 处理 iconfont
                 {
-                    test: /\.(svg|eot|ttf|woff)($|\?)/,
+                    test: /\.(svg|eot|ttf|woff|woff2)($|\?)/,
                     loader: 'file'
                 }
             ]
